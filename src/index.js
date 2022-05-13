e920fb1cbc27da5237bf5775ed488175ed869f02
Chris-McLeod2 <91762149+Chris-McLeod2@users.noreply.github.com>
2022-05-13 13:57:22 -0500

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import App from './App';
-import './styles.css'
-import ReactDomClient from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-const container = document.getElementById('root');
-const root = ReactDomClient.createRoot(container)
-root.render(<App />);
